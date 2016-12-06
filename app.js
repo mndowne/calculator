1fc3d5951cb95860ce356dc8684405464e03d1b0
mndowne <mndowne@gmail.com>
2016-12-05 21:43:04 -0600

    getting js to work

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.set('port',(process.env.PORT | 3000));
+
+app.use(express.static(__dirname));
+app.get('/', function(req,res){
+  res.render(__dirname + '/index.html');
+});
+
+app.listen(app.get('port'),function(){
+  console.log('server is up on process.env.PORT or 3000');
+});
